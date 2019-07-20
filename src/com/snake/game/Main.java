87ab02b21a2d168efec4636359d03462e896c47f
jeffbolton <i.otkhozoria@yahoo.com>
2019-07-20 15:58:30 +0400

    new project

---
+++ src/com/snake/game/Main.java
@@ -0,0 +1,9 @@
+package com.snake.game;
+
+public class Main {
+
+    public static void main(String[] args) {
+        new Universe();
+    }
+
+}
---
+++ src/com/snake/game/Universe.java
@@ -0,0 +1,18 @@
+package com.snake.game;
+
+import javax.swing.*;
+
+
+class Universe extends JFrame {
+
+
+    Universe() {
+        String GAME_TITLE = "Snake In Java";
+        setTitle(GAME_TITLE);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(320, 320);
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
+
+}
